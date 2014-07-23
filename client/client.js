97bb0446acf7a9d0c86d812911bd80f7817f9912
Fernando Marcheselli <fmarcheselli@gmail.com>
2014-07-22 18:53:31 -0700

    smooth scroll to anchor

--- client/client.js
+++ client/client.js
@@ -1,11 +1,14 @@
-Template.hello.greeting = function () {
-  return "Welcome to jackpine.";
-};
-
-Template.hello.events({
-  'click input': function () {
-    // template data, if any, is available in 'this'
-    if (typeof console !== 'undefined')
-      console.log("You pressed the button");
-  }
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    if (target.length) {
+      $('html,body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
+    }
+    }
+  });
 });
