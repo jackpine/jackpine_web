74400a33f264df2ff9bbf684ba90387c9856de22
Fernando Marcheselli <fmarcheselli@gmail.com>
2014-07-23 12:36:03 -0700

    background image centered and full bleed

--- client/javascript/client.js
+++ client/javascript/client.js
@@ -3,4 +3,22 @@ $(function() {
     panelSelector: '.full_page',
     $menu: '.nav-bar',
   });
+
+
+  function resizeBg() {
+   var $bg = $("#bg"),
+       aspectRatio = $bg.width() / $bg.height();
+
+    if ( ($(window).width() / $(window).height()) < aspectRatio ) {
+      $bg.removeClass().addClass('bgheight');
+      $bg.css({ marginLeft: -($bg.width()/2 - $(window).width()/2) });
+    } else {
+      $bg.removeClass().addClass('bgwidth');
+      $bg.css({ marginLeft: 0 });
+    }
+  }
+
+
+  $(window).resize(resizeBg).trigger("resize");
+
 });
